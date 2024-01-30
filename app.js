11af54137c599540a7a7ae2e8a1f6737d40e9367
CrimsonHog <crimsflare@gmail.com>
2024-01-30 15:59:13 -0600

    Tuesday

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 5500;
-
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5500;
+// set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-let myTypeServer = "The Eradictor";
+// use res.render to load up an ejs view file
 
-app.get('/', function(req,res){
-    res.render('index', {
-        
-    });
+let myTypeServer = "I dont know my enneagram";
+
+app.get('/', function(req, res) {
+
+  res.render('index', {
+   
+    myTypeClient: myTypeServer 
+
+  });
+  
 });
 
-app.get('/', function(req,res){
 
-    res.send('Hello world from express')
-});
+app.get('/send', function (req, res) {
+  
+    res.send('Hello World from Express <br><a href="">home</a>')
+})
 
-app.listen(port, () =>{
-    console.log(`listening on port ${port}`)
-}); 
+// app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`nov app listening on port ${port}`)
+})
