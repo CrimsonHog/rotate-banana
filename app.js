07ece38f47820c61319f015b6ca042660c3e90da
Crimson ... <crimsflare@gmail.com>
2024-02-13 16:46:23 -0600

    Fix dotenv

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
+// fix this
+require('dotenv').config();
+// ^^^^^^^^^
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 // use res.render to load up an ejs view file
 
@@ -19,6 +51,8 @@ app.get('/', function(req, res) {
   });
   
 });
+
+
 
 
 app.get('/send', function (req, res) {
